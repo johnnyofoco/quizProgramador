500ec4ba0a9e8d7f86fd2a4a2e06ce5f206d0cad
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-09-24 11:33:27 -0300

    Finished the question component and started the gameOver component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 // react, componentes, estáticos
-import { useContext } from 'react'
+import { useContext,useEffect } from 'react'
 import { QuizContext } from './context/quiz'
 
 import Welcome from './components/Welcome'
 import Question from './components/Question'
+import GameOver from './components/GameOver'
 
 import './App.css'
-import {useEffect} from 'react'
+
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext)
@@ -21,8 +22,8 @@ function App() {
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
-      
-      
+      {quizState.gameStage === "End" && <GameOver />}
+            
     </div>
   )
 }
