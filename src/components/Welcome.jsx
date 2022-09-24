9a4191a12040cfe88b0c941346cb64e44f566dd4
johnnyofoco <johnny.marlon.paula@gmail.com>
2022-09-24 10:23:24 -0300

    Include context and control stage with reduce

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,14 +1,20 @@
-import quiz from "../img/quiz.svg";
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
+
+import Quiz from "../img/quiz.svg";
 
 import "./Welcome.css"
 
 const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext)
+
+  
   return (
     <div id="welcome">
       <h2>Seja bem-vindo</h2>
       <p>Clique no botão abaixo para começar</p>
-      <button>Iniciar</button>
-      <img src={quiz} alt="Inicio do Quiz" />
+      <button onClick={()=> dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
+      <img src={Quiz} alt="Inicio do Quiz" />
     </div>
   )
 }
